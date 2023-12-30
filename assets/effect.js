d30304342460cb59bb24ac4539fcb41cacc55e82
Cuong <cuongchamchi1904@gmail.com>
2023-12-30 23:42:27 +0700

    final

--- assets/effect.js
+++ assets/effect.js
@@ -219,7 +219,7 @@ $('document').ready(function(){
 		$('#b66').animate({top:240, left: vw+150},500);
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
-		$('#b44').css('z-index','-20');
+		$('#b44').css('opacity','0');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
 			$('#story').fadeIn('slow');
