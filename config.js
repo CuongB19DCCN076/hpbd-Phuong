c6f5cddaa3447d0125342d07810ec749de9fa929
Cuong <cuongchamchi1904@gmail.com>
2023-12-30 22:27:28 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,22 @@
+var config = {
+    texts: [
+        "I wish you are always young,",      
+        "beautiful,",  
+        "lucky",
+        "and have a lot of success in your life.",
+        "Happy birthday to you",
+    ],
+    imgs: {
+        "beautiful": "./imgs/phuong.png",
+    },
+    desc: {
+        turn_on: "Bắt đầu",
+        play: "Nhạc nè",
+        bannar_coming: "Bấm tiếp nè",
+        balloons_flying: "Thiếu cái gì đó",
+        cake_fadein: "Bánh ư？",
+        light_candle: "Nến ư？",
+        wish_message: "Chúc mừng sinh nhật",
+        story: "Tin nhắn dành cho bạn",
+    }
+};
